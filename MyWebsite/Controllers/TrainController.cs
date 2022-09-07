e2b0c4be8883c964b827aee06a0700d913c749c7
Sampath <sampathsr88@gmail.com>
2022-09-07 23:26:56 +0530

    Intial code changes

---
+++ MyWebsite/Controllers/TrainController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    public class TrainController : Controller
+    {
+        //
+        // GET: /Train/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult AddTrain()
+        {
+            return View ()
+        }
+
+        public ActionResult SearchTrain()
+        {
+            return View();
+
+        }
+
+        public ActionResult EditTrain()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteTrain()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ MyWebsite/Models/clsTrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebsite.Models
+{
+    public class clsTrain
+    {
+        private Guid TrainID;
+        private string TrnName;
+        private string Origin;
+        private string Destination;
+        private DateTime ArrivalTime;
+
+    }
+}
